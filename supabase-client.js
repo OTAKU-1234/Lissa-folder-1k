23a6bb57e251aedd26fa154848b1941fc4e18fd5
OTAKU-1234 <garnolhensromulus12@gmail.com>
2026-09-03 12:14:25 -0400

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,11 +1,29 @@
+// =====================================================
+// LISSA 1K FOLDER — SUPABASE CLIENT
+// =====================================================
 
-const SUPABASE_URL = "https://grmqbvihmnzvgwufjmbt.supabase.co";
+const SUPABASE_URL =
+    "https://grmqbvihmnzvgwufjmbt.supabase.co";
 
-const SUPABASE_KEY = "sb_publishable_pDhX2wAEzv7fzTFIeFiyVA_YrqZPdVa";
+const SUPABASE_KEY =
+    "sb_publishable_pDhX2wAEzv7fzTFIeFiyVA_YrqZPdVa";
 
-const supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
+if (!window.supabase) {
+    console.error("Supabase JS n'est pas chargé.");
+} else {
 
-window.supabaseClient = supabaseClient;
+    const supabaseClient =
+        window.supabase.createClient(
+            SUPABASE_URL,
+            SUPABASE_KEY,
+            {
+                auth: {
+                    persistSession: true,
+                    autoRefreshToken: true,
+                    detectSessionInUrl: true
+                }
+            }
+        );
+
+    window.supabaseClient = supabaseClient;
+}
